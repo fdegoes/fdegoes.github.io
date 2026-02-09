22a6fa8daa3051fce7af76418c0c790076e50126
fernando <fernando@pixar.com>
2026-02-09 15:33:10 -0800

    claude notes

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ var options = {
 };
 
 var userList = new List('publications', options);
+
+// Update search results count
+userList.on('searchComplete', function() {
+  var resultsSpan = document.querySelector('.search-results');
+  var count = userList.visibleItems.length;
+  var total = userList.items.length;
+
+  if (userList.searched) {
+    resultsSpan.textContent = 'Showing ' + count + ' of ' + total + ' publications';
+  } else {
+    resultsSpan.textContent = '';
+  }
+});
