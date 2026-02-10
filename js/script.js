783a191d988f5f3e0a401c0ef4c0eb3972de27ea
fernando <fernando@pixar.com>
2026-02-10 10:39:33 -0800

    clear

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ var options = {
 
 var userList = new List('publications', options);
 
+// Get references to search input and clear button
+var searchInput = document.querySelector('.search');
+var clearButton = document.querySelector('.search-clear');
+
 // Update search results count
 userList.on('searchComplete', function() {
   var resultsSpan = document.querySelector('.search-results');
@@ -15,4 +19,29 @@ userList.on('searchComplete', function() {
   } else {
     resultsSpan.textContent = '';
   }
+
+  // Show/hide clear button based on search input
+  toggleClearButton();
 });
+
+// Show/hide clear button based on input value
+function toggleClearButton() {
+  if (searchInput.value.length > 0) {
+    clearButton.classList.add('visible');
+  } else {
+    clearButton.classList.remove('visible');
+  }
+}
+
+// Handle clear button click
+clearButton.addEventListener('click', function() {
+  searchInput.value = '';
+  userList.search();
+  searchInput.focus();
+});
+
+// Monitor input changes to show/hide clear button
+searchInput.addEventListener('input', toggleClearButton);
+
+// Initial check on page load
+toggleClearButton();
